1a3a9f35ce9dc11812a98dcdf81a8cc405f1f3ad
Joseph <joseph@verifiedjoseph.com>
2019-09-04 12:28:42 +0000

    [ViceBridge] Change mkdir mode (0755 -> 0775) (#26)

--- bridges/ViceBridge.php
+++ bridges/ViceBridge.php
@@ -153,7 +153,7 @@ es_latam, de_at, be, pt_br, fr, fr_be, de, gr, id_id, it, jp, nl, pt, ar',
 	private function loadCache() {
 
 		if (is_dir($this->cacheFolder) === false) {
-			mkdir($this->cacheFolder, 0755);
+			mkdir($this->cacheFolder, 0775);
 		}
 
 		$path = $this->cacheFolder . '/' . $this->cacheName();
