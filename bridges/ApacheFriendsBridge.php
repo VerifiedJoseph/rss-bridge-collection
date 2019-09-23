4d300bef4e3dabcddce9e41c21cbaf546e266ae2
Joseph <joseph@verifiedjoseph.com>
2019-09-23 11:18:09 +0000

    [ApacheFriendsBridge] Add Bridge (#28)
    * [ApacheFriendsBridge] Add Bridge
    * [README] Add Apache Friends bridge

---
+++ bridges/ApacheFriendsBridge.php
@@ -0,0 +1,43 @@
+<?php
+class ApacheFriendsBridge extends FeedExpander {
+
+	const NAME = 'Apache Friends Bridge';
+	const URI = 'https://www.apachefriends.org';
+	const DESCRIPTION = 'Returns newest XAMPP releases';
+	const MAINTAINER = 'VerifiedJoseph';
+
+	const CACHE_TIMEOUT = 3600; // 1 hour
+
+	protected function parseItem($item) {
+
+		// Use published date instead of updated date for the timestamp. 
+		// The updated date value is changed every time a new item is added to the feed, making it useless.
+		$published = $item->published;
+
+		$item = parent::parseItem($item);
+
+		// Fix broken URLs
+		$item['uri'] = str_replace('http://blog.url.com', self::URI, $item['uri']);
+		$item['content'] = defaultLinkTo($item['content'], $this->getURI());
+
+		// Change author from 'Article Author' to 'Apache Friends'.
+		$item['author'] = 'Apache Friends';
+
+		// Set timestamp as published date.
+		$item['timestamp'] = $published;
+
+		return $item;
+	}
+
+	public function collectData() {
+		$this->collectExpandableDatas($this->getURI() . '/feed.xml', 20);
+	}
+
+	public function getName() {
+		return 'Apache Friends';
+	}
+
+	public function getURI() {
+		return self::URI;
+	}
+}
