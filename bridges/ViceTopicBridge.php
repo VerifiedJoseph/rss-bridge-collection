e889ca6af2f010974aa392552e21f8c6477da1a3
Joseph <joseph@verifiedjoseph.com>
2020-06-13 20:13:34 +0100

    [ViceTopicBridge] Minor changes

--- bridges/ViceTopicBridge.php
+++ bridges/ViceTopicBridge.php
@@ -7,7 +7,6 @@
 	The name and location of the cache folder can be changed by modifying the '$cacheFolder' variable.
 */
 class ViceTopicBridge extends BridgeAbstract {
-
 	const NAME = 'Vice.com Topic Bridge';
 	const URI = 'https://vice.com';
 	const DESCRIPTION = 'Returns the newest articles for a topic from multiple vice.com editions.';
@@ -69,7 +68,6 @@ es_latam, de_at, be, pt_br, fr, fr_be, de, gr, id_id, it, jp, nl, pt, ar',
 	);
 
 	public function collectData() {
-
 		$this->editions = explode(',', $this->getInput('editions'));
 		$this->editions = array_map('trim', $this->editions);
 		$this->editions = array_unique($this->editions);
@@ -139,7 +137,6 @@ es_latam, de_at, be, pt_br, fr, fr_be, de, gr, id_id, it, jp, nl, pt, ar',
 	}
 
 	private function orderItems() {
-
 		$sort = array();
 
 		foreach ($this->items as $key => $item) {
@@ -170,7 +167,6 @@ es_latam, de_at, be, pt_br, fr, fr_be, de, gr, id_id, it, jp, nl, pt, ar',
 	}
 
 	private function saveCache() {
-
 		$contents = json_encode($this->cache);
 
 		$path = $this->cacheFolder . '/' . $this->cacheName();
